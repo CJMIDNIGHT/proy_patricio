4230afdb60541498dee800f55e61a2a36c97cb66
Adenor <amburet@epsg.upv.es>
2026-03-25 19:58:42 +0100

    mundo creado

---
+++ patricio_my_world/launch/house.launch.py
@@ -0,0 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg = get_package_share_directory('patricio_my_world')
+    world = os.path.join(pkg, 'worlds', 'house.sdf')
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': world}.items()
+    )
+
+    return LaunchDescription([gz_sim])
